932001111f8c58d973ec6807b8bdcf6afbc0e6eb
jinhyuck8504 <jinhyucks@gmail.com>
2025-07-27 08:31:14 +0900

    Delete app/page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50 p-8">
-      <h1 className="text-4xl font-bold text-blue-600">🏛️ 미니처치</h1>
-      <p className="text-xl text-gray-600">테스트 페이지입니다</p>
-    </div>
-  )
-}
